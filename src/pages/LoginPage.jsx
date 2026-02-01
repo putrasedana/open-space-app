3193e635bd1740bb34bf20f782dcac49c15cd944
Putrasedana <putrasedana03@gmail.com>
2026-02-02 00:15:34 +0800

    make test pass

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -16,24 +16,18 @@ function LoginPage() {
   return (
     <section className="login-page">
       <header className="login-page__hero">
-        <h1><IoEarthOutline /></h1>
+        <h1>
+          <IoEarthOutline />
+        </h1>
       </header>
       <article className="login-page__main">
         <h2>
-          See
-          {' '}
-          <strong>The World</strong>
-          ,
-          {' '}
-          <br />
-          Through Open Space.
+          See <strong>The World</strong>, <br />
+          Through Open Space App.
         </h2>
-
         <LoginInput login={onLogin} />
         <p>
-          Don&apos;t have an account?
-          {' '}
-          <Link to="/register">Register</Link>
+          Don&apos;t have an account? <Link to="/register">Register</Link>
         </p>
       </article>
     </section>
